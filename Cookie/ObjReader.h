26208636244e58a031a9a8f1f615dbf3c63497d1
Samuel Proulx <sproulx100@outlook.com>
2019-10-23 14:29:04 -0400

    Remove template from ObjReader

--- Cookie/ObjReader.h
+++ Cookie/ObjReader.h
@@ -11,25 +11,6 @@ namespace Cookie
 	public:
 		static ObjData Read(std::string const& filePath);
 	private:
-		template <class TIteratable>
-		static void Split(TIteratable const& buf, char delimiter, std::vector<std::string_view>& result);
+		static void Split(std::string_view buf, char delimiter, std::vector<std::string_view>& result);
 	};
-
-	template <class TIteratable>
-	void ObjReader::Split(TIteratable const& buf, char delimiter, std::vector<std::string_view>& result)
-	{
-		result.clear();
-		auto it = begin(buf);
-		auto it2 = begin(buf);
-		while ((it2 = find(it, end(buf), delimiter)) != end(buf))
-		{
-			result.push_back(std::string_view{ &(*it), static_cast<size_t>(it2 - it) });
-			it = it2 + 1;
-		}
-
-		if (it != it2)
-		{
-			result.push_back(std::string_view{ &(*it), static_cast<size_t>(it2 - it) });
-		}
-	}
 }
