5910b17b5f7aaa7e611b9ee1c8b19300c2060bcf
Victor Bodin <victor.bodin@free.fr>
2019-12-13 17:31:28 -0500

    Correction Build

--- Cookie/CookieMath.h
+++ Cookie/CookieMath.h
@@ -8,5 +8,11 @@ namespace Cookie
         constexpr double Pi = 3.14159265358979323846;
 
 		bool COOKIE_API Approximately(float a, float b);
+    	
+		template<class T, class U>
+		static auto Lerp(T a, T b, U f)
+		{
+			return a + static_cast<float>(f)* (b - a);
+		}
     }
 }
