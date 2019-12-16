2e485a19805a0658079269c167b5cfe19809954a
Victor Bodin <victor.bodin@free.fr>
2019-12-16 14:55:45 -0500

    HUDLogic

--- Cookie/GuiManager.h
+++ Cookie/GuiManager.h
@@ -47,9 +47,7 @@ namespace Cookie
 		static void InitText();
 		static void CloseText();
 
-		void SetPosDim(Sprite* sprite, int x_, int y_, int dx_, int dy_) const;
-
-		void setButtonPosDim(Sprite* sprite, int x_, int y_, int dx_, int dy_) const;
+		void SetPosDim(Sprite* sprite, int x_, int y_, int dx_, int dy_, int xDimPix, int yDimPix) const;
 	public:
 		uint32_t ScreenHeight;
 		uint32_t ScreenWidth;
@@ -57,9 +55,9 @@ namespace Cookie
 		GuiManager(TextureManager* textureManager, InputManager* inputManager, DeviceD3D11* device);
 		~GuiManager();
 		
-		Sprite* newSprite(const std::string& textureName, int xPos, int yPos, int xDim = 1, int yDim = 1, bool bouton = false);
+		Sprite* newSprite(const std::string& textureName, int xPos, int yPos, int xScale = 1, int yScale = 1, int xDimPix = 0, int yDimPix = 0);
 		Text* newText(int width, int height, Gdiplus::Font* font, const std::wstring& text_, int xPos, int yPos);
-		Button* newButton(const std::string& textureBackgroung, const std::string& textureOver, int width, int height, Gdiplus::Font* pFont, const std::wstring& text_, int xPos, int yPos, std::function<void()> action = {}, int offsetTextBoutonX = 0, int offsetTextBoutony = 0);
+		Button* newButton(const std::string& textureBackgroung, const std::string& textureOver, int width, int height, Gdiplus::Font* pFont, const std::wstring& text_, int xPos, int yPos, std::function<void()> action = []() {}, int offsetTextBoutonX = 0, int offsetTextBoutony = 0);
 		void deleteGuiElement(Sprite* sprite);
 
 		void changeSpriteTexture(const std::string& textureName, Sprite* sprite) const;
