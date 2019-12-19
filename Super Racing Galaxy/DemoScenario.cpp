5d42552da5e27277d7c2e2e8eafa38896ea4d4e0
Gloubii <36268562+Gloubii@users.noreply.github.com>
2019-12-19 17:18:58 -0500

    on passe plus à travers les planètes

--- Super Racing Galaxy/DemoScenario.cpp
+++ Super Racing Galaxy/DemoScenario.cpp
@@ -252,7 +252,7 @@ Scenario ScenarioCreator::CreateDemoScenario()
 	planete4->addElement(piste5);
 	
 
-	scenario.tpLinks.push_back(TeleportLinksParams{ teleporteur3, planete2, piste1, planete1, {false, Cookie::Vector3<>{}}, {false, Cookie::Vector3<>{}}, {} });
+	scenario.tpLinks.push_back(TeleportLinksParams{ teleporteur3, planete2, piste1, planete1, {true, Cookie::Vector3<>{18.0f, 16.0f, 19.0f} +2 * Cookie::Vector3<>{12.0f,25.0f,40.0f}}, {false, Cookie::Vector3<>{}}, {} });
 	scenario.tpLinks.push_back(TeleportLinksParams{ teleporteur1, planete1, piste3, planete3, {false, Cookie::Vector3<>{}}, {false, Cookie::Vector3<>{}}, {} });
 	scenario.tpLinks.push_back(TeleportLinksParams{ teleporteur2, planete1, piste5, planete4, {false, Cookie::Vector3<>{}}, {false, Cookie::Vector3<>{}}, {} });
 	scenario.tpLinks.push_back(TeleportLinksParams{ teleporteur5, planete4, piste4, planete3, {false, Cookie::Vector3<>{}}, {false, Cookie::Vector3<>{}}, {} });
